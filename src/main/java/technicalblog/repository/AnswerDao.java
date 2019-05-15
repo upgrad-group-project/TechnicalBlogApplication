d3a0832679d637b0d5d4e3a03e667337aa906e17
vandana064 <49123644+vandana064@users.noreply.github.com>
2019-05-15 11:42:02 +0530

    AnswerDao.java created

---
+++ src/main/java/technicalblog/repository/AnswerDao.java
@@ -0,0 +1,61 @@
+package technicalblog;
+
+import com.upgrad.quora.service.entity.AnswerEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class AnswerDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public AnswerEntity createAnswer(AnswerEntity answerEntity)
+    {
+        entityManager.persist(answerEntity);
+        return answerEntity;
+    }
+
+    public AnswerEntity getAnswerById(String uuid)
+    {
+        try {
+             return entityManager.createNamedQuery("getAnswerFromId", AnswerEntity.class).setParameter("uuid", uuid)
+                    .getSingleResult();
+        }catch (NoResultException nre)
+        {
+            return null;
+        }
+    }
+
+    public AnswerEntity checkAnswerBelongToUser(String auuid, String uuuid)
+    {
+
+        try {
+            return entityManager.createNamedQuery("checkAnswerBelongToUser", AnswerEntity.class).setParameter("auuid", auuid).setParameter("uuuid",uuuid).getSingleResult();
+        }catch (NoResultException nre)
+        {
+            return null;
+        }
+    }
+
+    public AnswerEntity updateAnswer(AnswerEntity answerEntity)
+    {
+        return entityManager.merge(answerEntity);
+    }
+
+    public AnswerEntity deleteAnswer(AnswerEntity answerEntity)
+    {
+        entityManager.remove(answerEntity);
+        return answerEntity;
+    }
+
+    public List<AnswerEntity> getAllAnswers(String questionId)
+    {
+        return entityManager.createNamedQuery("getAllAnswers",AnswerEntity.class)
+                .setParameter("uuid",questionId).getResultList();
+    }
+}
